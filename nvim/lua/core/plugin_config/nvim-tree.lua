3b7250f15406ca92a4fd33a65cace83e06974020
BleskoDev <bleskodev@ennumia.eu>
2023-11-10 11:13:49 +0100

    Upgrade nvim-tree configuration to remove the warning.
    view.mappings configuration entry has been removed. Move the mapping to
    on_attach function.

--- nvim/lua/core/plugin_config/nvim-tree.lua
+++ nvim/lua/core/plugin_config/nvim-tree.lua
@@ -8,15 +8,22 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- key bindings
+local function nvt_on_attach(bufnr)
+    local api = require "nvim-tree.api"
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set('n', 'u', api.tree.change_root_to_parent, { desc = "nvim-tree: Up", buffer = bufnr, noremap = true, silent = true, nowait = true })
+end
+
 nvim_tree.setup({
     sort_by = "case_sensitive",
+    on_attach = nvt_on_attach,
     view = {
-        width = 30,
-        mappings = {
-            list = {
-                { key = "u", action = "dir_up" },
-            },
-        },
+        width = 30
     },
 })
 
